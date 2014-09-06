327458bfd316d49495b8d5a12e6a3957f7ab3b3f
bagiroff <oleg.bagiroff@gmail.com>
2014-09-06 18:54:48 +0300

    edit js

--- Team 3/script.js
+++ Team 3/script.js
@@ -2,12 +2,10 @@ function submitform(){
 	var text=document.getElementById('text').value;
 	if (text!==''){
 			var parent = document.getElementById('events');
-			var NewDiv =document.createElement('div');
 			var newElem = document.createElement('li');
 			newElem.innerHTML = 'Подія<!-- додати лічильник --><span class="tip">' + text + '</span>';
-			parent.appendChild(NewDiv);
-			NewDiv.setAttribute("onclick", "remove();");
-			NewDiv.appendChild(newElem);
+			newElem.setAttribute("onclick", "remove();");
+			parent.appendChild(newElem);
 			document.getElementById('text').value='';
 			document.getElementById('err_text').innerHTML='';
 	}
