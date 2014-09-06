086b0f6a4d18773bdaf7efb1d1c037e3004846de
bagiroff <oleg.bagiroff@gmail.com>
2014-09-06 17:21:47 +0300

    допилил удаление элементов правда как-то через костыли:)

--- Team 3/script.js
+++ Team 3/script.js
@@ -2,9 +2,12 @@ function submitform(){
 	var text=document.getElementById('text').value;
 	if (text!==''){
 			var parent = document.getElementById('events');
+			var NewDiv =document.createElement('div');
 			var newElem = document.createElement('li');
 			newElem.innerHTML = 'Подія<!-- додати лічильник --><span class="tip">' + text + '</span>';
-			parent.appendChild(newElem);
+			parent.appendChild(NewDiv);
+			NewDiv.setAttribute("onclick", "remove();");
+			NewDiv.appendChild(newElem);
 			document.getElementById('text').value='';
 			document.getElementById('err_text').innerHTML='';
 	}
@@ -12,4 +15,9 @@ function submitform(){
 	else {
 		document.getElementById('err_text').innerHTML='(введіть текст події)';
 	}
+}
+function remove(){
+	var elem = parentElem.this;
+	var parent= parentElem.elem;
+	parent.removeChild();
 }
