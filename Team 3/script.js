80f58f6184a7cc0893d8b7b90287145c13dea5e7
bagiroff <oleg.bagiroff@gmail.com>
2014-09-06 11:35:15 +0300

    хтмл, яваскрипт

---
+++ Team 3/script.js
@@ -0,0 +1,20 @@
+function textcheck(){
+var text=document.getElementById('text');
+if (text.value!=='') { 
+document.getElementById('err_text').innerHTML='';
+return text.value;} 
+else {
+document.getElementById('err_text').innerHTML='* Введіть текст події'; return false;}
+}
+function submitform(){
+var text = textcheck();
+if (!!text){
+		var parent = document.getElementById('new_event');
+		var newElem = document.createElement('li');
+		newElem.innerHTML = 'Подія<!-- додати лічильник --><span id="tip">' + text + '</span>';
+		parent.appendChild(newElem);
+		document.getElementById('text').value='';
+		document.getElementById('err_text').innerHTML='';}
+
+else {document.getElementById('err_text').innerHTML='* Введіть текст події';}
+}
