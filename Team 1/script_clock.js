9d738b2b0fee407089b08a7ecfa07d6af31fba79
anabnk <anastasia.buniak@gmail.com>
2014-09-08 00:52:27 +0300

    final changes

--- Team 1/script_clock.js
+++ Team 1/script_clock.js
@@ -7,14 +7,15 @@ function Watch(){
        var hours = date.getHours();
        var minutes = date.getMinutes();
        var seconds = date.getSeconds();
- 
+	
+	hours=hours>12?hours-=12:hours
 
   
-    hoursArrow.style.webkitTransform = 'rotate('+(hours+(minutes*1.6666666)/100)*30+'deg)'; 
-    hoursArrow.style.mozTransform    = 'rotate('+(hours+(minutes*1.6666666)/100)*30+'deg)'; 
-    hoursArrow.style.msTransform     = 'rotate('+(hours+(minutes*1.6666666)/100)*30+'deg)'; 
-    hoursArrow.style.oTransform      = 'rotate('+(hours+(minutes*1.6666666)/100)*30+'deg)'; 
-    hoursArrow.style.transform       = 'rotate('+(hours+(minutes*1.6666666)/100)*30+'deg)'; 
+    hoursArrow.style.webkitTransform = 'rotate('+(hours+minutes*0.0166666)*30+'deg)'; 
+    hoursArrow.style.mozTransform    = 'rotate('+(hours+minutes*0.0166666)*30+'deg)'; 
+    hoursArrow.style.msTransform     = 'rotate('+(hours+minutes*0.0166666)*30+'deg)'; 
+    hoursArrow.style.oTransform      = 'rotate('+(hours+minutes*0.0166666)*30+'deg)'; 
+    hoursArrow.style.transform       = 'rotate('+(hours+minutes*0.0166666)*30+'deg)'; 
 
     minutesArrow.style.webkitTransform = 'rotate('+minutes*6+'deg)'; 
     minutesArrow.style.mozTransform    = 'rotate('+minutes*6+'deg)'; 
@@ -28,4 +29,3 @@ function Watch(){
     secondsArrow.style.oTransform      = 'rotate('+seconds*6+'deg)'; 
     secondsArrow.style.transform       = 'rotate('+seconds*6+'deg)'; 
     }
-
