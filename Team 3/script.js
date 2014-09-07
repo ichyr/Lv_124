967efbff770aab81053cb634c04c74d76644f963
Measim <l_eugene@inbox.ru>
2014-09-07 12:28:14 +0300

    some code optimization and close on red cross

--- Team 3/script.js
+++ Team 3/script.js
@@ -1,16 +1,33 @@
 function submitform(){
-	var text=document.getElementById('text').value;
+	var text = document.getElementById('text').value;
+	var actionCount, actionId ;
+	
 	if (text!==''){
 			var parent = document.getElementById('events');
 			var newElem = document.createElement('li');
-			newElem.innerHTML = 'Подія<!-- додати лічильник --><span class="tip">' + text + '</span>';
-			newElem.setAttribute("onclick", "remove();");
-			parent.appendChild(newElem);
+
+            actionCount =  parent.getElementsByTagName('li').length + 1;
+            actionId = randomizer(-9999, 9999) + "id";
+			newElem.innerHTML ='Подія ' +  actionCount + '<span class ="closeAction" id ="' + actionId + '"' + '> </span>' + '<span class="tip">' + text + '</span>';
+            parent.appendChild(newElem);
+			parent = document.getElementById(actionId);
+			parent.addEventListener("click", removeAction);
 			document.getElementById('text').value='';
 			document.getElementById('err_text').innerHTML='';
 	}
-
 	else {
-		document.getElementById('err_text').innerHTML='(введіть текст події)';
-	}
-}
+	  document.getElementById('err_text').innerHTML='(введіть текст події)';
+	};
+};
+
+function removeAction(){
+ var parent = document.getElementById('events');
+ parent.removeChild(this.parentNode); 
+};
+
+function randomizer(max, min) {
+   var rand = min - 0.5 + Math.random()*(max-min+1);
+   
+    rand = Math.round(rand);
+   return rand;
+};
